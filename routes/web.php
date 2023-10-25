2c0471db27c0bc1778de5ba55f7a647e3a14de56
KurodaKJ <jack.kaeorahan@student.gildeopleidingen.nl>
2023-10-25 10:16:52 +0200

    Fix layouts.app not found

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/search', function () {
+Route::get('/', function () {
     return view('search');
 })->name('search');
+
 Route::get('/test', function () {
     return 'Test route is working!';
 });
+
+Route::get('/results', function () {
+    return view('results');
+})->name('search.results');
 
 
