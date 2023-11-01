978287060cd5d19406dd74646c56db82136c396a
KurodaKJ <jack.kaeorahan@student.gildeopleidingen.nl>
2023-11-01 20:58:18 +0100

    Add search box to result page for futher searching

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -4,6 +4,15 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <p class="text-muted">Vind wat u zoekt.</p>
+            <form action="{{ route('search.results') }}" method="GET" class="mt-3">
+                <div class="input-group mb-3">
+                    <input type="text" name="query" class="form-control" placeholder="Voer uw zoekopdracht in...">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit">Zoeken</button>
+                    </div>
+                </div>
+            </form>
             <h2>Zoekresultaten voor "{{ $query }}"</h2>
             <hr>
             <ul class="list-group">
